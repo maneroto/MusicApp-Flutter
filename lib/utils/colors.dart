21e49879592948bad98ba6e76f16f0055930dc54
maneroto <maneroto@hotmail.com>
2021-01-07 16:53:48 -0600

    Add const files and variables

---
+++ lib/utils/colors.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+const primary = const Color(0x360033); // Purple
+const secondary = const Color(0x0B8793); // Blue
+
+const primaryA = const Color(0x3600333C); // Purple 60%
+const secondaryA = const Color(0x0B87933C); // Blue 60%
+
+const primaryAccent = const Color(0x2A264E); // Dark Purple
+const secondaryAccent = const Color(0x204463); //  Dark Blue
+
+const primaryAccentA = const Color(0x2A264E3C); // Dark Purple 60%
+const secondaryAccentA = const Color(0x2044633C); //  Dark Blue 60%
+
+/**
+ * Atoms to be created:
+ * Heading @Ernek
+ * Text @Mane
+ * Icon @Ernek
+ * Button @Mane
+ * IconButton @Ernek
+ * Image @Mane
+ */
---
+++ lib/utils/textStyle.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+import './colors.dart';
+
+import 'package:flutter/cupertino.dart';
+
+const styleH1 = TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 32.0,
+    color: primaryAccent);
+
+const styleH2 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 24.0,
+    color: primaryA);
+
+const styleH3 = const TextStyle(
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.bold,
+    fontSize: 20.0,
+    color: primaryA);
+
+const styleText = const TextStyle(
+    fontFamily: 'Lato',
+    fontWeight: FontWeight.normal,
+    fontSize: 16.0,
+    color: primaryAccent);
+
+const styleTextSmall = const TextStyle(
+    fontFamily: 'Lato',
+    fontWeight: FontWeight.normal,
+    fontSize: 12.0,
+    color: primaryAccent);
