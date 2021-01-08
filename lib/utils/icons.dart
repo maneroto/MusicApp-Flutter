07cbafa7b2ea4e344da6cce2d26070cdae488743
Ernesto Cabañas <71567859+ernesto-ca@users.noreply.github.com>
2021-01-07 20:42:05 -0600

    Images, icons and first atoms added

---
+++ lib/utils/icons.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const delete = Icon(Icons.delete);
+const search = Icon(Icons.search);
+const close = Icon(Icons.close);
+const playing = Icon(Icons.album);
+const cancel = Icon(Icons.cancel);
+const favorite = Icon(Icons.favorite);
+const noFavorite = Icon(Icons.favorite_border);
+const viewList = Icon(Icons.view_list);
+const filter = Icon(Icons.filter_list);
+const menu = Icon(Icons.menu);
+const loop = Icon(Icons.loop);
+const random = Icon(Icons.swap_horiz);
+const play = Icon(Icons.play_arrow);
+const pause = Icon(Icons.pause);
+const next = Icon(Icons.skip_next);
+const previous = Icon(Icons.skip_previous);
